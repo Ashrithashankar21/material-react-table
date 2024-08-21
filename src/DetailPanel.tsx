b85ec3e02ef8ee5de6e1152ec72a1943329f255b
Ashritha Shankar <ashritha.shankar@solitontech.com>
2024-08-21 17:30:06 +0530

    feat: src: use code editor
    Signed-off-by: Ashritha Shankar <ashritha.shankar@solitontech.com>

--- src/DetailPanel.tsx
+++ src/DetailPanel.tsx
@@ -1,16 +1,28 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Button } from "@mui/material";
 import OtherDetailsTable from "./OtherDetailsTable.tsx"; // Import the updated table component
- 
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/ext-language_tools";
+import 'ace-builds/src-noconflict/theme-monokai';
+
 function DetailPanel({ row, data, setData }) {
-  const [code, setCode] = useState(row.original.otherDetails.code);
- 
-  const handleCodeChange = (event) => {
-    setCode(event.target.value);
+  const [code, setCode] = useState<string>(row.original.otherDetails.code); 
+  const [isModified, setIsModified] = useState(false);
+
+  const handleCodeChange = (newCode:string) => {
+    console.log('event',newCode);
+
+    setCode(newCode);
+    setIsModified(true);
+
   };
  
   const handleSave = () => {
+    console.log('data',data);
     const updatedData = data.map((item, index) => {
+      console.log('index',index,row.id,item);
       if (index === parseInt(row.id)) {
         return {
           ...item,
@@ -19,10 +31,18 @@ function DetailPanel({ row, data, setData }) {
       }
       return item;
     });
+    console.log('updatedData',updatedData);
     setData(updatedData);
+    setIsModified(false);
   };
+
+  useEffect(() => {
+    setIsModified(code !== row.original.otherDetails.code);
+  }, [code, row.original.otherDetails.code]);
+
+
   const otherDetails = row.original.otherDetails;
- 
+
   return (
     <div
       style={{
@@ -41,21 +61,22 @@ function DetailPanel({ row, data, setData }) {
           gap: "8px",
         }}
       >
-        <textarea
+  
+        <AceEditor
+          mode="java"
+          theme="monokai"
           value={code}
           onChange={handleCodeChange}
-          rows={10}
-          style={{
-            fontFamily: "monospace",
-            fontSize: "14px",
-            padding: "10px",
-          }}
+          name="UNIQUE_ID_OF_DIV"
+          editorProps={{ $blockScrolling: true }}
+          width="100%"
         />
         <Button
           onClick={handleSave}
           variant="contained"
           color="primary"
           style={{ width: "fit-content" }}
+          disabled={!isModified}
         >
           Save
         </Button>
